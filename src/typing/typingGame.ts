a2159dc20daf4300094f29dfdaab5c089ae01ce4
shsk000 <02luvsic@gmail.com>
2024-11-11 00:59:31 +0900

    feat: return result information

--- src/typing/typingGame.ts
+++ src/typing/typingGame.ts
@@ -3,15 +3,39 @@ import { AlphabetInputPattern } from "./alphabetInputPattern";
 import { InputValidator } from "./inputValidator";
 
 type AnswerResult = {
-  result: "correct" | "incorrect" | "finish";
+  result: "correct" | "fail" | "complate";
+  /** １タイプごと：入力失敗数 */
+  failCount: number;
+  /** １タイプごと：入力成功数 */
+  correctCount: number;
+  /** 入力が完了した文章数 */
+  completedSentences: number;
+  /** １タイプごと：入力失敗数 */
+  perfectStreakCount: number;
 };
 
 export class TypingGame {
   private inputPattern: string[] = [];
   private inputValidator: InputValidator;
 
+  /** １タイプごと：入力失敗数 */
+  private failCount: number = 0;
+  /** １タイプごと：入力成功数 */
+  private correctCount: number = 0;
+  /** 入力が完了した文章数 */
+  private completedSentences: number = 0;
+  /** １タイプごと：入力失敗数 */
+  private perfectStreakCount: number = 0;
+
   constructor() {
     this.inputValidator = new InputValidator();
+  }
+
+  public initialize() {
+    this.failCount = 0;
+    this.correctCount = 0;
+    this.completedSentences = 0;
+    this.perfectStreakCount = 0;
   }
 
   public registerNewHiragana(hiragana: string) {
@@ -25,19 +49,38 @@ export class TypingGame {
     const { result } = this.inputValidator.input(alphabet);
 
     if (result === "correct") {
+      this.correctCount++;
       return {
         result: "correct",
+        failCount: this.failCount,
+        correctCount: this.correctCount,
+        completedSentences: this.completedSentences,
+        perfectStreakCount: this.perfectStreakCount,
       };
     }
 
-    if (result === "finish") {
+    if (result === "complate") {
+      this.correctCount++;
+      this.completedSentences++;
+      this.perfectStreakCount++;
+
       return {
-        result: "finish",
+        result: "complate",
+        failCount: this.failCount,
+        correctCount: this.correctCount,
+        completedSentences: this.completedSentences,
+        perfectStreakCount: this.perfectStreakCount,
       };
     }
 
+    this.failCount++;
+    this.perfectStreakCount = 0;
     return {
-      result: "incorrect",
+      result: "fail",
+      failCount: this.failCount,
+      correctCount: this.correctCount,
+      completedSentences: this.completedSentences,
+      perfectStreakCount: this.perfectStreakCount,
     };
   }
 }

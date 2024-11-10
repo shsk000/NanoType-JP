14ef6ba8eee462d55f74e813eb3587b413301e9e
shsk000 <shk2xen@gmail.com>
2024-11-10 23:49:13 +0900

    feat: input validator

--- src/typing/typingGame.ts
+++ src/typing/typingGame.ts
@@ -1,10 +1,15 @@
 import { createRomajiSentence } from "../parseHiragana/romajiSentence";
+import { AlphabetInputPattern } from "./alphabetInputPattern";
 
 export class TypingGame {
+  private inputPattern: string[] = [];
+
   constructor() {}
 
   public registerNewHiragana(hiragana: string) {
     const romajiSentence = createRomajiSentence(hiragana);
+    const pattern = new AlphabetInputPattern(romajiSentence);
+    this.inputPattern = pattern.getAllPatern();
   }
 
   public answerAlphabet(alphabet: string) {}

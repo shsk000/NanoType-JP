28089438270145b6e73a159e612c87c5bbfc0624
shsk000 <02luvsic@gmail.com>
2024-11-17 01:12:53 +0900

    feat: export setting

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,21 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
-
+import { resolve } from "path";
+import dtsPlugin from "vite-plugin-dts";
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
+export default defineConfig(({ mode }) => {
+  return {
+    base: "./",
+
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/index.ts"),
+        name: "index",
+        fileName: "index",
+        formats: ["es", "umd", "cjs"],
+      },
+    },
+
+    plugins: [dtsPlugin()],
+  };
 });

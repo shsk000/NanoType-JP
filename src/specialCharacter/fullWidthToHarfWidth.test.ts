6ef06ffdb392a0abbabb3690a79a46c3ca04b514
shsk000 <shk2xen@gmail.com>
2024-11-24 23:09:45 +0900

    feat: 全角変換処理の追加

---
+++ src/specialCharacter/fullWidthToHarfWidth.test.ts
@@ -0,0 +1,54 @@
+import { describe, expect, test } from "vitest";
+import { fullWidthToHalfWidth } from "./fullWidthToHarfWidth";
+
+describe("fullWidthToHarfWidth", () => {
+  const testCases = [
+    { input: "！", expected: "!" },
+    { input: '"', expected: '"' },
+    { input: "＃", expected: "#" },
+    { input: "＄", expected: "$" },
+    { input: "％", expected: "%" },
+    { input: "＆", expected: "&" },
+    { input: "'", expected: "'" },
+    { input: "（", expected: "(" },
+    { input: "）", expected: ")" },
+    { input: "＝", expected: "=" },
+    { input: "～", expected: "~" },
+    { input: "｜", expected: "|" },
+    { input: "｛", expected: "{" },
+    { input: "｝", expected: "}" },
+    { input: "'", expected: "'" },
+    { input: "＊", expected: "*" },
+    { input: "＋", expected: "+" },
+    { input: "＿", expected: "_" },
+    { input: "？", expected: "?" },
+    { input: "＜", expected: "<" },
+    { input: "＞", expected: ">" },
+    { input: "、", expected: "," },
+    { input: "。", expected: "." },
+    { input: "￥", expected: "\\" },
+    { input: "；", expected: ";" },
+    { input: "：", expected: ":" },
+    { input: "」", expected: "]" },
+    { input: "ー", expected: "-" },
+    { input: "「", expected: "[" },
+    { input: "＠", expected: "@" },
+    { input: "＾", expected: "^" },
+    { input: "－", expected: "-" },
+    { input: "　", expected: "" },
+  ];
+
+  test.each(testCases)(
+    `converts $input to $expected`,
+    ({ input, expected }) => {
+      console.log(input, expected);
+      expect(fullWidthToHalfWidth(input)).toBe(expected);
+    }
+  );
+
+  test("converts multiple characters in a sentence", () => {
+    const input = '全角文字："＠＃＄％＆"';
+    const expected = '全角文字:"@#$%&"';
+    expect(fullWidthToHalfWidth(input)).toBe(expected);
+  });
+});
